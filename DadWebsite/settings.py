78b2ea89c0f49047de6aa0bb04994bd67106d9cc
George Burns <georgeburnsdev@gmail.com>
2022-12-09 17:42:05 +0000

    import heroku

--- DadWebsite/settings.py
+++ DadWebsite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,5 @@ MEDIA_ROOT= BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())

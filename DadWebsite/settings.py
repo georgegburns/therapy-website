6202bb35c726f6641803a5ea41f5c7656aad8694
georgegburns <88833145+georgegburns@users.noreply.github.com>
2022-12-09 20:10:46 +0000

    Update settings.py

--- DadWebsite/settings.py
+++ DadWebsite/settings.py
@@ -77,7 +77,12 @@ WSGI_APPLICATION = 'DadWebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = DATABASE_URL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
